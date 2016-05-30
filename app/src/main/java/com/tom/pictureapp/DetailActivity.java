7633a9c9eca22d64130e8576e5357ca145efa388
Hank Tom <hanktom@gmail.com>
2016-05-30 22:00:19 +0800

    GestureDetector

--- app/src/main/java/com/tom/pictureapp/DetailActivity.java
+++ app/src/main/java/com/tom/pictureapp/DetailActivity.java
@@ -8,18 +8,22 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.widget.ImageView;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements GestureDetector.OnGestureListener{
 
     private int position;
     private Cursor cursor;
     private ImageView image;
+    private GestureDetector detector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        detector = new GestureDetector(this, this);
         position = getIntent().getIntExtra("POS", 0);
         Log.d("POS", position +"");
         CursorLoader loader = new CursorLoader(this,
@@ -28,11 +32,54 @@ public class DetailActivity extends AppCompatActivity {
         cursor = loader.loadInBackground();
         cursor.moveToPosition(position);
         image = (ImageView) findViewById(R.id.imageView);
+        updateImage();
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return detector.onTouchEvent(event);
+    }
+
+    private void updateImage() {
         String path = cursor.getString(
                 cursor.getColumnIndex(MediaStore.Images.Media.DATA));
         Log.d("PATH", path);
         Bitmap bitmap = BitmapFactory.decodeFile(path);
         image.setImageBitmap(bitmap);
+    }
 
+    @Override
+    public boolean onDown(MotionEvent e) {
+        Log.d("GESTURE", "onDown");
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+        Log.d("GESTURE", "onShowPress");
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        Log.d("GESTURE", "onSingleTapUp");
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        Log.d("GESTURE", "onScroll");
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+        Log.d("GESTURE", "onLongPress");
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        Log.d("GESTURE", "onFling");
+        return false;
     }
 }
