2da29b8c840b431038cc3795264630bdb90abb02
Hank Tom <hanktom@gmail.com>
2016-05-30 21:31:10 +0800

    點縮圖後顯示原圖

--- app/src/main/java/com/tom/pictureapp/DetailActivity.java
+++ app/src/main/java/com/tom/pictureapp/DetailActivity.java
@@ -1,12 +1,20 @@
 package com.tom.pictureapp;
 
+import android.content.CursorLoader;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
 public class DetailActivity extends AppCompatActivity {
 
     private int position;
+    private Cursor cursor;
+    private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +22,17 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         position = getIntent().getIntExtra("POS", 0);
         Log.d("POS", position +"");
-        
+        CursorLoader loader = new CursorLoader(this,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                null, null, null, null);
+        cursor = loader.loadInBackground();
+        cursor.moveToPosition(position);
+        image = (ImageView) findViewById(R.id.imageView);
+        String path = cursor.getString(
+                cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+        Log.d("PATH", path);
+        Bitmap bitmap = BitmapFactory.decodeFile(path);
+        image.setImageBitmap(bitmap);
+
     }
 }
